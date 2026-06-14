4dbe409a29f949ac876c5df6e1453ba46618a3e6
matheusburey <matheusbureidev@gmail.com>
2026-06-14 09:09:38 -0300

    chore: remove backend Node.js project

--- src/repositories/PricesRepository.ts
+++
@@ -1,9 +0,0 @@
-export interface PriceData {
-	origin: string;
-	destiny: string;
-	value?: string;
-}
-
-export interface PriceRepository {
-	findValue: (data: PriceData) => Promise<{ value: number } | null>;
-}
